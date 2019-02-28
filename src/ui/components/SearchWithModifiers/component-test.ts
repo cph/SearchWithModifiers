d34c22b865502f4c49c322df69fddddfadd54284
Matthew Kobs <matt.kobs@cph.org>
2019-02-28 11:49:50 -0600

    [squash] Updated test to address deprecation (1m)

--- src/ui/components/SearchWithModifiers/component-test.ts
+++ src/ui/components/SearchWithModifiers/component-test.ts
@@ -1,5 +1,5 @@
 import hbs from '@glimmer/inline-precompile';
-import { setupRenderingTest } from '@glimmer/test-helpers';
+import { render, setupRenderingTest } from '@glimmer/test-helpers';
 
 const { module, test } = QUnit;
 
@@ -7,7 +7,7 @@ module('Component: SearchWithModifiers', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    await this.render(hbs`<SearchWithModifiers />`);
+    await render(hbs`<SearchWithModifiers />`);
     assert.ok(this.containerElement.querySelector('div.search'));
     assert.ok(this.containerElement.querySelector('div.search-box'), 'Expected search box');
   });
